1f66cd54f71b9ca97de350d6df28430c81ef703c
Mauro <mauro@pop-os.localdomain>
2022-03-04 19:03:08 -0600

    fixing background

--- src/pages/shop.vue
+++ src/pages/shop.vue
@@ -21,7 +21,7 @@ const { cakes } = storeToRefs(store);
 <style>
 .bg {
   padding: 3rem;
-  background-image: url("src/assets/bg.png");
+  background-image: url("../assets/bg.png");
   background-position: center;
   background-repeat: no-repeat;
   background-size: cover;
